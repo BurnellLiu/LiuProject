1d1006faa582b94a9300d3682351ea974a519e58
BurnellLiu <burnell_liu@outlook.com>
2018-04-25 17:16:00 +0800

    完善

--- SigSlot/SigSlot/LSignalSlot.h
+++ SigSlot/SigSlot/LSignalSlot.h
@@ -9,36 +9,57 @@ using std::list;
 
 class LSlot;
 
-
-template<typename TParam>
-class LConnectionBase
+/// @brief 没有参数的链接基类模板
+class LConnectionBase0
 {
 public:
-    virtual ~LConnectionBase() {}
+    virtual ~LConnectionBase0() {}
+
+    /// @brief 获取槽对象
     virtual LSlot* GetDest() const = 0;
-    virtual void EmitSignal(TParam) = 0;
+
+    /// @brief 发送信号
+    virtual void EmitSignal() = 0;
 };
 
 
-template<typename TObject, typename TParam>
-class LConnection : public LConnectionBase<TParam>
+/// @brief 带有一个参数的链接基类模板
+template<typename TParam>
+class LConnectionBase1
 {
-    typedef void (TObject::*TFunc)(TParam);
 public:
-    LConnection(TObject* pObject, TFunc pFunc)
+    virtual ~LConnectionBase1() {}
+
+    /// @brief 获取槽对象
+    virtual LSlot* GetDest() const = 0;
+
+    /// @brief 发送信号
+    virtual void EmitSignal(TParam) = 0;
+};
+
+/// @brief 没有参数的链接类模板
+template<typename TObject>
+class LConnection0 : public LConnectionBase0
+{
+    typedef void (TObject::*TFunc)();
+public:
+    /// @brief 构造函数
+    LConnection0(TObject* pObject, TFunc pFunc)
     {
         m_pObject = pObject;
         m_pFunc = pFunc;
     }
 
-    void EmitSignal(TParam param)
-    {
-        (m_pObject->*m_pFunc)(param);
-    }
-
+    /// @brief 获取槽对象
     LSlot* GetDest() const
     {
         return m_pObject;
+    }
+
+    /// @brief 发送信号
+    void EmitSignal()
+    {
+        (m_pObject->*m_pFunc)();
     }
 
 private:
@@ -46,58 +67,85 @@ private:
     TFunc m_pFunc;          // 链接对象成员函数
 };
 
-/// <SUMMARY>
-/// 信号基类接口
-/// </SUMMARY>
-class LSignalBaseI
+/// @brief 带有1个参数的链接类模板
+template<typename TObject, typename TParam>
+class LConnection1 : public LConnectionBase1<TParam>
+{
+    typedef void (TObject::*TFunc)(TParam);
+public:
+    /// @brief 构造函数
+    LConnection1(TObject* pObject, TFunc pFunc)
+    {
+        m_pObject = pObject;
+        m_pFunc = pFunc;
+    }
+
+    /// @brief 获取槽对象
+    LSlot* GetDest() const
+    {
+        return m_pObject;
+    }
+
+    /// @brief 发送信号
+    void EmitSignal(TParam param)
+    {
+        (m_pObject->*m_pFunc)(param);
+    }
+
+private:
+    TObject* m_pObject;     // 链接对象
+    TFunc m_pFunc;          // 链接对象成员函数
+};
+
+
+/// @brief 信号基类
+class LSignalBase
 {
 public:
-    virtual ~LSignalBaseI() {}
+    virtual ~LSignalBase() {}
 
-    /// <SUMMARY>
-    /// 槽要求断开链接
-    /// </SUMMARY>
+    /// @brief 槽要求断开链接
     virtual void SlotDisconnect(LSlot* pSlot) = 0;
 };
 
 
-/// <SUMMARY>
-/// 槽类
-/// </SUMMARY>
+/// @brief 槽类
+/// 
+/// 槽对象中保存有一个信号集合, 以便在槽对象析构时, 
+/// 调用信号对象断开链接, 防止槽对象删除后, 
+/// 信号对象依然呼叫槽函数
+/// 
+/// 抽象出基类LSignalBase供槽类使用更方便
 class LSlot
 {
-    typedef set<LSignalBaseI*> LSignalsSet;
+    typedef set<LSignalBase*> LSignalsSet;
 
 public:
+    /// @brief 构造函数
     LSlot()
     {
 
     }
 
+    /// @brief 析构函数
     ~LSlot()
     {
         this->DisconnectAll();
     }
 
-    /// <SUMMARY>
-    /// 信号要求进行链接
-    /// </SUMMARY>
-    void SignalConnect(LSignalBaseI* pSignal)
+    /// @brief 信号要求进行链接
+    void SignalConnect(LSignalBase* pSignal)
     {
         m_signalsSet.insert(pSignal);
     }
 
-    /// <SUMMARY>
-    /// 信号要求断开链接
-    /// </SUMMARY>
-    void SignalDisconnect(LSignalBaseI* pSignal)
+    /// @brief 信号要求断开链接
+    void SignalDisconnect(LSignalBase* pSignal)
     {
         m_signalsSet.erase(pSignal);
     }
 
-    /// <SUMMARY>
-    /// 断开所有连接
-    /// </SUMMARY>
+    /// @brief 断开所有连接
     void DisconnectAll()
     {
         for (auto iter = m_signalsSet.begin(); iter != m_signalsSet.end(); iter++)
@@ -113,32 +161,53 @@ private:
 };
 
 
-/// <SUMMARY>
-/// 信号基类
-/// </SUMMARY>
-template<typename TParam>
-class LSignalBase : public LSignalBaseI
+/// @brief 没有参数的信号类
+/// 
+/// 信号对象中需要保存槽对象和槽函数
+/// 槽对象和槽函数可以为不同类实例
+/// 所以我们需要使用链接类对它们进行封装
+class LSignal0 : public LSignalBase
 {
-    typedef list<LConnectionBase<TParam>*> LConnectionsList;
+    typedef list<LConnectionBase0*> LConnectionsList;
 
 public:
-    LSignalBase()
+    /// @brief 构造函数
+    LSignal0()
     {
 
     }
 
-    ~LSignalBase()
+    /// @brief 析构函数
+    ~LSignal0()
     {
         this->DisconnectAll();
     }
 
-    /// <SUMMARY>
-    /// 断开指定槽
-    /// </SUMMARY>
+    /// @brief 链接槽
+    template<typename TObject>
+    void Connect(TObject* pObject, void (TObject::*pFunc)())
+    {
+        LConnection0<TObject>* pConn = new LConnection0<TObject>(pObject, pFunc);
+        m_connsList.push_back(pConn);
+
+        pObject->SignalConnect(this);
+    }
+
+    /// @brief 激发信号
+    void Emit()
+    {
+        for (auto iter = m_connsList.begin(); iter != m_connsList.end(); iter++)
+        {
+            (*iter)->EmitSignal();
+        }
+    }
+
+    /// @brief 断开指定槽
     void Disconnect(LSlot* pSlot)
     {
         /*
-        主动断开槽时, 需要在槽中也断开信号
+        信号主动断开链接时, 需要在槽中也断开与信号的链接,
+        并且删除链接对象
         */
 
         auto iter = m_connsList.begin();
@@ -159,30 +228,143 @@ public:
         }
     }
 
-    /// <SUMMARY>
-    /// 断开所有链接
-    /// </SUMMARY>
+    /// @brief 断开所有链接
     void DisconnectAll()
     {
         /*
-        主动断开槽时, 需要在槽中也断开信号
+        信号主动断开链接时, 需要在槽中也断开与信号的链接
+        并且删除链接对象
         */
 
         for (auto iter = m_connsList.begin(); iter != m_connsList.end(); iter++)
         {
             (*iter)->GetDest()->SignalDisconnect(this);
+
+            delete *iter;
         }
 
         m_connsList.clear();
     }
 
-    /// <SUMMARY>
-    /// 槽要求断开链接
-    /// </SUMMARY>
+    /// @brief 槽要求断开链接
     void SlotDisconnect(LSlot* pSlot)
     {
         /*
-        被槽要求断开时, 槽自己处理断开信号
+        槽要求断开链接时, 槽自己会处理断开与信号的链接
+        */
+
+        auto iter = m_connsList.begin();
+        while (iter != m_connsList.end())
+        {
+            if ((*iter)->GetDest() == pSlot)
+            {
+                delete *iter;
+                iter = m_connsList.erase(iter);
+            }
+            else
+            {
+                iter++;
+            }
+
+        }
+    }
+
+private:
+    LConnectionsList m_connsList; // 链接列表
+};
+
+
+/// @brief 带有一个参数的信号类模板
+/// 
+/// 信号对象中需要保存槽对象和槽函数
+/// 槽对象和槽函数可以为不同类实例
+/// 所以我们需要使用链接类对它们进行封装
+template<typename TParam>
+class LSignal1 : public LSignalBase
+{
+    typedef list<LConnectionBase1<TParam>*> LConnectionsList;
+
+public:
+    /// @brief 构造函数
+    LSignal1()
+    {
+
+    }
+
+    /// @brief 析构函数
+    ~LSignal1()
+    {
+        this->DisconnectAll();
+    }
+
+    /// @brief 链接槽
+    template<typename TObject>
+    void Connect(TObject* pObject, void (TObject::*pFunc)(TParam))
+    {
+        LConnection1<TObject, TParam>* pConn = new LConnection1<TObject, TParam>(pObject, pFunc);
+        m_connsList.push_back(pConn);
+
+        pObject->SignalConnect(this);
+    }
+
+    /// @brief 激发信号
+    void Emit(TParam param)
+    {
+        for (auto iter = m_connsList.begin(); iter != m_connsList.end(); iter++)
+        {
+            (*iter)->EmitSignal(param);
+        }
+    }
+
+    /// @brief 断开指定槽
+    void Disconnect(LSlot* pSlot)
+    {
+        /*
+        信号主动断开链接时, 需要在槽中也断开与信号的链接,
+        并且删除链接对象
+        */
+
+        auto iter = m_connsList.begin();
+        while (iter != m_connsList.end())
+        {
+            if ((*iter)->GetDest() == pSlot)
+            {
+                delete *iter;
+                iter = m_connsList.erase(iter);
+
+                pSlot->SignalDisconnect(this);
+            }
+            else
+            {
+                iter++;
+            }
+
+        }
+    }
+
+    /// @brief 断开所有链接
+    void DisconnectAll()
+    {
+        /*
+        信号主动断开链接时, 需要在槽中也断开与信号的链接
+        并且删除链接对象
+        */
+
+        for (auto iter = m_connsList.begin(); iter != m_connsList.end(); iter++)
+        {
+            (*iter)->GetDest()->SignalDisconnect(this);
+
+            delete *iter;
+        }
+
+        m_connsList.clear();
+    }
+
+    /// @brief 槽要求断开链接
+    void SlotDisconnect(LSlot* pSlot)
+    {
+        /*
+        槽要求断开链接时, 槽自己会处理断开与信号的链接
         */
 
         auto iter = m_connsList.begin();
@@ -201,31 +383,9 @@ public:
         }
     }
 
-protected:
+private:
     LConnectionsList m_connsList; // 链接列表
 };
 
-
-template<typename TParam>
-class LSignal : public LSignalBase<TParam>
-{
-public:
-    template<typename TObject>
-    void Connect(TObject* pObject, void (TObject::*pFunc)(TParam))
-    {
-        LConnection<TObject, TParam>* pConn = new LConnection<TObject, TParam>(pObject, pFunc);
-        m_connsList.push_back(pConn);
-
-        pObject->SignalConnect(this);
-    }
-
-    void operator()(TParam param)
-    {
-        for (auto iter = m_connsList.begin(); iter != m_connsList.end(); iter++)
-        {
-            (*iter).EmitSignal(param);
-        }
-    }
-};
 
 
--- SigSlot/SigSlot/main.cpp
+++ SigSlot/SigSlot/main.cpp
@@ -1,7 +1,54 @@
 
+#include <cstdio>
+
+#include "LSignalSlot.h"
+
+
+class Button
+{
+public:
+    LSignal0 ClickSignal;
+
+    void NowClick(bool b)
+    {
+        this->ClickSignal.Emit();
+    }
+};
+
+class Tv : public LSlot
+{
+public:
+    Tv(int i) 
+    {
+        m_id = i;
+    }
+
+    void OnButtonClicked()
+    {
+        
+        printf("Tv %d is being turned on\n", m_id);
+    }
+
+private:
+    int m_id;
+};
 
 
 int main()
 {
+    Button btn;
+
+    Tv* tv1 = new Tv(1);
+    Tv* tv2 = new Tv(2);
+
+    btn.ClickSignal.Connect(tv1, &Tv::OnButtonClicked);
+    btn.ClickSignal.Connect(tv2, &Tv::OnButtonClicked);
+
+    //btn.ClickSignal.Disconnect(tv1);
+    // delete tv1;
+    btn.NowClick(true);
+
+    system("pause");
+
     return 0;
 }
